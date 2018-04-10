d6a2418e3d9a0c9db979f3b6cde7e3d6bb82e2c9
Teleportal <joshthegreenberg@gmail.com>
2018-04-09 20:12:59 -0500

    Made Footer Year Dynamic

--- public/assets/js/front.js
+++ public/assets/js/front.js
@@ -1,28 +1,28 @@
 /*global $, document, Chart, LINECHART, data, options, window*/
-$(document).ready(function () {
-
-    'use strict';
+$(document).ready(function() {
+    "use strict";
 
     // Main Template Color
-    var brandPrimary = '#33b35a';
+    var brandPrimary = "#33b35a";
 
     // ------------------------------------------------------- //
     // For demo purposes only
     // ------------------------------------------------------ //
 
     if ($.cookie("theme_csspath")) {
-        $('link#theme-stylesheet').attr("href", $.cookie("theme_csspath"));
+        $("link#theme-stylesheet").attr("href", $.cookie("theme_csspath"));
     }
 
-    $("#colour").change(function () {
+    $("#colour").change(function() {
+        if ($(this).val() !== "") {
+            var theme_csspath = "css/style." + $(this).val() + ".css";
 
-        if ($(this).val() !== '') {
+            $("link#theme-stylesheet").attr("href", theme_csspath);
 
-            var theme_csspath = 'css/style.' + $(this).val() + '.css';
-
-            $('link#theme-stylesheet').attr("href", theme_csspath);
-
-            $.cookie("theme_csspath", theme_csspath, { expires: 365, path: document.URL.substr(0, document.URL.lastIndexOf('/')) });
+            $.cookie("theme_csspath", theme_csspath, {
+                expires: 365,
+                path: document.URL.substr(0, document.URL.lastIndexOf("/"))
+            });
         }
 
         return false;
@@ -35,67 +35,71 @@ $(document).ready(function () {
     if ($(window).outerWidth() > 992) {
         $("nav.side-navbar").niceScroll({
             cursorcolor: brandPrimary,
-            cursorwidth: '3px',
-            cursorborder: 'none'
+            cursorwidth: "3px",
+            cursorborder: "none"
         });
     }
-
 
     // ------------------------------------------------------- //
     // Side Navbar Functionality
     // ------------------------------------------------------ //
-    $('#toggle-btn').on('click', function (e) {
-
+    $("#toggle-btn").on("click", function(e) {
         e.preventDefault();
 
         if ($(window).outerWidth() > 1194) {
-            $('nav.side-navbar').toggleClass('shrink');
-            $('.page').toggleClass('active');
+            $("nav.side-navbar").toggleClass("shrink");
+            $(".page").toggleClass("active");
         } else {
-            $('nav.side-navbar').toggleClass('show-sm');
-            $('.page').toggleClass('active-sm');
+            $("nav.side-navbar").toggleClass("show-sm");
+            $(".page").toggleClass("active-sm");
         }
     });
-
 
     // ------------------------------------------------------- //
     // Login  form validation
     // ------------------------------------------------------ //
-    $('#login-form').validate({
+    $("#login-form").validate({
         messages: {
-            loginUsername: 'please enter your username',
-            loginPassword: 'please enter your password'
+            loginUsername: "please enter your username",
+            loginPassword: "please enter your password"
         }
     });
 
     // ------------------------------------------------------- //
     // Register form validation
     // ------------------------------------------------------ //
-    $('#register-form').validate({
+    $("#register-form").validate({
         messages: {
-            registerUsername: 'please enter your first name',
-            registerEmail: 'please enter a vaild Email Address',
-            registerPassword: 'please enter your password'
+            registerUsername: "please enter your first name",
+            registerEmail: "please enter a vaild Email Address",
+            registerPassword: "please enter your password"
         }
     });
 
     // ------------------------------------------------------- //
     // Transition Placeholders
     // ------------------------------------------------------ //
-    $('input').on('focus', function () {
-        $(this).siblings('.label-custom').addClass('active');
+    $("input").on("focus", function() {
+        $(this)
+            .siblings(".label-custom")
+            .addClass("active");
     });
 
-    $('input').on('blur', function () {
-        $(this).siblings('.label-custom').removeClass('active');
+    $("input").on("blur", function() {
+        $(this)
+            .siblings(".label-custom")
+            .removeClass("active");
 
-        if ($(this).val() !== '') {
-            $(this).siblings('.label-custom').addClass('active');
+        if ($(this).val() !== "") {
+            $(this)
+                .siblings(".label-custom")
+                .addClass("active");
         } else {
-            $(this).siblings('.label-custom').removeClass('active');
+            $(this)
+                .siblings(".label-custom")
+                .removeClass("active");
         }
     });
-
 
     // ------------------------------------------------------- //
     // Jquery Progress Circle
@@ -105,16 +109,14 @@ $(document).ready(function () {
         line_width: 5,
         percent: 80
     });
-    progress_circle.gmpc('animate', 80, 3000);
+    progress_circle.gmpc("animate", 80, 3000);
 
     // ------------------------------------------------------- //
     // External links to new window
     // ------------------------------------------------------ //
 
-    $('.external').on('click', function (e) {
-
+    $(".external").on("click", function(e) {
         e.preventDefault();
         window.open($(this).attr("href"));
     });
-
 });
